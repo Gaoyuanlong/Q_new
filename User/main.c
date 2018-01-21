ff2b2c68a11839ec51062e4fe0cddb35f52f3847
Gaoyuanlong <1002925853@qq.com>
2018-01-21 10:30:05 +0800

    test3

--- User/main.c
+++ User/main.c
@@ -19,7 +19,7 @@ void Get_Time(void)
 
 //-------------------------------------------//
 int main(void)
-{//test2
+{//test3
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  
 	SystemTime.Init();
 	Led.Init();
